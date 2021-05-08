135e09ae18528df6fa7089d135668ec77e2e976f
JerryDot <dansinh64@gmail.com>
2021-05-08 11:30:38 +0100

    initial commit

---
+++ fetch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Fetches the input, prints to stdout and copies to clipboard.
+# This gives both a quick overview of what the input looks like
+# and makes it available on ctrl+v for use in the challenge.
+# do ./fetch.sh <SESSION_TOKEN>
+
+if [ -z "$1" ]; then
+  echo "No session token."
+  exit 1
+fi
+
+for x in {1..25}
+do
+    URL="https://adventofcode.com/2017/day/$x/input"
+    curl $URL --cookie "session=$1" -s > /src/input/input$x.txt
+done
