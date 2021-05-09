0e843243d501cd553444f97019c67cff67246f58
bar <foo>
2021-05-09 09:59:06 +0100

    finish day 2 (TODO tidy) + yml and notes

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -5,24 +5,24 @@ static INPUT: &str = include_str!("../input/input2.txt");
 fn parse_input(input_string: &str) -> Vec<Vec<u32>> {
     input_string
         .trim()
-        .split("\n")
+        .split('\n')
         .map(|s| s
-            .split("\t")
+            .split('\t')
             .map(|w| w
                 .parse::<u32>()
-                .expect(&format!("{} wasn't a valid u32", w)))
+                .unwrap_or_else(|w| panic!("Err {}  - wasn't a valid u32", w)))
                 .collect::<Vec<u32>>())
         .collect::<Vec<Vec<u32>>>()
 }
 
-fn part_one(input_array: &Vec<Vec<u32>>) -> u32 {
+fn part_one(input_array: &[Vec<u32>]) -> u32 {
     input_array
         .iter()
         .map(|l| l.iter().max().unwrap() - l.iter().min().unwrap())
     .sum()
 }
 
-fn part_two(input_array: &Vec<Vec<u32>>) -> f64 {
+fn part_two(input_array: &[Vec<u32>]) -> f64 {
     let x = input_array
         .iter()
         .map(|l| l
@@ -38,9 +38,6 @@ fn part_two(input_array: &Vec<Vec<u32>>) -> f64 {
         .map(|t| {
             let z = &t[0];
             *z[0].max(z[1]) as f64 / *z[0].min(z[1]) as f64
-            // println!("{:?}", z);
-            // // z[1] as f64 / z[0] as f64
-            // 4 as f64
         })
         .sum();
     y
@@ -51,4 +48,26 @@ advent_of_rust::main! {
     let part_one = part_one(&cleaned_input);
     let part_two = part_two(&cleaned_input);
     (part_one, part_two)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one_answer() {
+        assert_eq!(
+            part_one(&parse_input(INPUT)),
+            36174
+        )
+    }
+
+    #[test]
+    fn part_two_answer() {
+        assert_eq!(
+            part_two(&parse_input(INPUT)) as u16,
+            244
+        )
+    }
 }
